ef1b0594f481c960f73b3420a09f5a4d4fc203db
ksophocleous <ksophocleous@gmail.com>
2014-06-02 23:01:52 +0200

    initial commit; modified include file location and adjusted source file include directive; library is now build using cmake

---
+++ include/sole/sole.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+#include <ostream>
+
+namespace sole
+{
+    // 128-bit basic UUID type that allows comparison and sorting.
+    // Use .str() for printing and .pretty() for pretty printing.
+    struct uuid
+    {
+        uint64_t ab;
+        uint64_t cd;
+
+        bool operator==( const uuid &other ) const;
+        bool operator!=( const uuid &other ) const;
+        bool operator <( const uuid &other ) const;
+
+		std::string compressed() const;
+        std::string pretty() const;
+        std::string base62() const;
+		std::string normal() const;
+        std::string str() const;
+    };
+
+    // Generators
+    uuid uuid0(); // UUID v0, pro: unique; cons: MAC revealed, pid revealed, predictable.
+    uuid uuid1(); // UUID v1, pro: unique; cons: MAC revealed, predictable.
+    uuid uuid4(); // UUID v4, pros: anonymous, fast; con: uuids "can clash"
+
+    // Rebuilders
+    uuid rebuild( uint64_t ab, uint64_t cd );
+    uuid rebuild( const std::string &uustr );
+}
+
+// ostream friendly
+std::ostream &operator<<( std::ostream &os, const sole::uuid &u );
